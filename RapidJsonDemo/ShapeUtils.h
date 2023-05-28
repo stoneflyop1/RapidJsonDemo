aff01a7218a8f289481214545f3c58e37c333c05
Jifeng <jeff-zhang@outlook.com>
2023-05-28 14:25:31 +0800

    refactor

--- RapidJsonDemo/ShapeUtils.h
+++ RapidJsonDemo/ShapeUtils.h
@@ -4,24 +4,31 @@
 #include <memory>
 #include "Shapes.h"
 
+inline std::unique_ptr<Shape> ParseShape(rapidjson::Value& item) {
+	const char* typeNameStr = Shape::ParseTypeName(item);
+	if (typeNameStr == nullptr) return nullptr;
+	std::string typeName = typeNameStr;
+	if (typeName == ShapeTypeNameCircle) {
+		auto c = std::make_unique<Circle>();
+		if (c->Deserialize(item)) {
+			return c;
+		}
+	}
+	else if (typeName == ShapeTypeNameSquare) {
+		auto s = std::make_unique<Square>();
+		if (s->Deserialize(item)) {
+			return s;
+		}
+	}
+	return nullptr;
+}
+
 inline bool ParseShapes(rapidjson::Value& json, std::vector<std::unique_ptr<Shape>>& shapes) {
 	if (!json.IsArray()) return false;
 	for (rapidjson::SizeType i = 0; i < json.Size(); i++) {
-		auto& item = json[i];
-		const char* typeNameStr = Shape::ParseTypeName(item);
-		if (typeNameStr == nullptr) continue;
-		std::string typeName = typeNameStr;
-		if (typeName == ShapeTypeNameCircle) {
-			auto c = std::make_unique<Circle>();
-			if (c->Deserialize(item)) {
-				shapes.push_back(std::move(c));
-			}
-		}
-		else if (typeName == ShapeTypeNameSquare) {
-			auto s = std::make_unique<Square>();
-			if (s->Deserialize(item)) {
-				shapes.push_back(std::move(s));
-			}
+		auto shape = ParseShape(json[i]);
+		if (shape != nullptr) {
+			shapes.push_back(std::move(shape));
 		}
 	}
 	return true;
